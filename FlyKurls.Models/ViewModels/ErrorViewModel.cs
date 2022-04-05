0b99999cb9d6c4af8b50a5e96c5cb47d96344b97
Channing Robertson <channing.robertson@gmail.com>
2022-04-05 06:55:58 -0500

    Add project files.

---
+++ FlyKurls.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FlyKurls2._0.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
